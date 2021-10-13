09fbbf17e9b23f5b906d51c573c5aef2aed13e74
山川恵斗 <20c1020051yy@edu.tech.ac.jp>
2021-10-13 10:22:25 +0900

    あ

--- kadai/kadai/kadai.cpp
+++ kadai/kadai/kadai.cpp
@@ -1,43 +1,63 @@
-﻿//第1問
-/*
-16
-97
-76
+﻿#include <stdio.h>
+//【問題】
+//メモリ確保を使用し、
+//1000個の配列を確保して、
+//その配列に0～999を設定するプログラムを作成してください。
+//newとdeleteで配列を確保する処理を、
+//クラスにしてデストラクタで自動でdeleteが呼ばれるようにしてください。
 
-第2問
-510
-253
-1584
 
-第3問
-1バイト
-2バイト
-4バイト
-4バイト
-
-第4問
-①
-
-第5問
-34
-
-第6問
-20
-
-第7問
-20
-
-第8問
-20
-
-第9問
-void
-
-第10問
-float a()
+class Hoge
 {
-float b , c, d;
+public:
+   
+    Hoge(int a)
+    {
+        hoge = new int[a];
+    };
 
-d = b - c;
+    ~Hoge()
+    {
+        delete[] hoge;
+        hoge = nullptr;
+    };
+    void Set_Hoge(int a, int b)
+    {
+        if (0 <= a && a < hogeSize)
+        {
+            hoge[a] = b;
+        }
+        else
+        {
+            printf("配列外参照です\n");
+        }
+    };
+
+    int Get_Hoge(int a)
+    {
+        if (0 <= a && a < hogeSize)
+        {
+            return hoge[a];
+        }
+        else
+        {
+            printf("配列外参照です\n");
+            return 0;
+        }
+    };
+
+    private:
+    int* hoge = nullptr;
+    int hogeSize;
+};
+int main()
+{
+    Hoge hoge(1000);
+
+    for (int i = 0; i < 1000; ++i)
+    {
+        hoge.Set_Hoge(i, i);
+
+        printf("num = %d\n", hoge.Get_Hoge(i));
+    }
 }
-*/
