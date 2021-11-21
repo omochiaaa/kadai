46feaac493518f0ff922480f540e7e33d61a68f3
山川恵斗 <20c1020051yy@edu.tech.ac.jp>
2021-11-21 23:16:08 +0900

    a

--- mondai/a/main.cpp
+++ mondai/a/main.cpp
@@ -3,12 +3,60 @@
 class Object 
 {
 public:
-    virtual void Update(float num)
-    {
-        x += num;
-    };
-private:
     float x = 0;
     float y = 0;
     float z = 0;
+
+	virtual void Update()
+	{
+		printf("x = %f\n", x);
+		printf("y = %f\n", y);
+		printf("z = %f\n", z);
+	};
+
+private:
+};
+class Vehicle : public Object
+{
+public:
+
+	virtual void SpeedUp()
+	{
+		speed += accel;
+	}
+
+	virtual void Update()
+	{
+		printf("x=%f\n", speed);
+	}
+
+	int speed = 1;
+	int	accel = 1;
+
+private:
+
+};
+class Car : public Vehicle
+{
+public:
+	Car()
+	{
+		speed = 1;
+		accel = 1;
+	};
+	void Update()
+	{
+		Vehicle::Update();
+	}
+private:
+};
+int main()
+{
+	Car car;
+
+	for (int i = 0; i < 10; i++)
+	{
+		car.SpeedUp();
+		car.Update();
+	}
 };
